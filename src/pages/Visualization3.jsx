eb9512df40df82674dcc069b9390b934759132e0
cencije <cencije@lafayette.edu>
2017-11-10 11:25:49 -0500

    Adding the file for the third visualization.

---
+++ src/pages/Visualization3.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Container, Image, Header, Icon, Dropdown, Divider } from 'semantic-ui-react'
+
+class Visualization3 extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Container fluid>
+
+        <Container>
+          <Header as='h1'>Hello Joe</Header>
+        </Container>
+
+      </Container>
+    )
+
+  }
+
+export default Visualization3
