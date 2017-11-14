efaedd54556ce1a415a60a1c8ec875f94cc6b2dc
nortondj <nortondj@lafayette.edu>
2017-11-13 23:29:23 -0500

    Create Visualization2.jsx

---
+++ src/pages/Visualization2.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Container, Header, p } from 'semantic-ui-react'
+import D3 from 'react-d3-core'
+import data from '../data/data_02.js'
+
+class Visualization2 extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount () {
+
+  }
+
+  render () {
+    return (
+        <Container className='vis2'>
+
+        </Container>
+    )
+  }
+
+}
+
+export default Visualization2
