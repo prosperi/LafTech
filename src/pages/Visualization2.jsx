7b52b4979a7d243457c3d6eaba45ddf3ada9f061
nortondj <nortondj@lafayette.edu>
2017-11-21 00:36:31 -0500

    Add interactive hover

--- src/pages/Visualization2.jsx
+++ src/pages/Visualization2.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Header } from 'semantic-ui-react'
 //import * as d3 from 'd3'
-import {hierarchy, partition} from 'd3-hierarchy'
+import {hierarchy, partition, style} from 'd3-hierarchy'
 import {select, selectAll} from 'd3-selection'
 import {arc} from 'd3-shape'
 import {color} from 'd3-color'
@@ -11,11 +11,12 @@ class Visualization2 extends Component {
     super(props)
 
     this.state = {
-      nodeData: {},
+      root: null,
+      hovered: null
     }
   }
 
-  componentDidMount () {
+  componentWillMount () {
     fetch('../data/data_02_process.json')
       .then(response => {
         if (response.status !== 200) {
@@ -23,17 +24,23 @@ class Visualization2 extends Component {
           return
         }
         response.json().then(data => {
+
+          // Find data root
+          let root = hierarchy(data)
+              .sum(function (d) { return d.size});
+
           this.setState({
-            nodeData: data
+            root: root
           })
         })
       })
   }
 
   render () {
-    // letiables
-    let width = 500;
-    let height = 500;
+
+    if(!this.state.root){
+      return (<div className="visContainer"></div>);
+    }
     let radius = Math.min(this.props.width, this.props.height) / 2;
 
     const colors = {
@@ -49,44 +56,23 @@ class Visualization2 extends Component {
             'Writing': '#458962',
             'SciBio': '#125592'
           }
-
-    // Create primary <g> element
-    /*select('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');*/
-
     // Data strucure
     let varPartition = partition()
         .size([2 * Math.PI, radius]);
-    // Find data root
-    let root = hierarchy(this.state.nodeData)
-        .sum(function (d) { return d.size});
 
     // Size arcs
-    varPartition(root);
+    varPartition(this.state.root);
 
     let arcGenerator = arc()
         .startAngle(function (d) { return d.x0 })
         .endAngle(function (d) { return d.x1 })
         .innerRadius(function (d) { return d.y0 })
         .outerRadius(function (d) { return d.y1 });
-    // Put it all together
-    /*
-    g.selectAll('path')
-        .data(root.descendants())
-        .enter()
-        .append('path')
-        .attr('display', function (d) { return d.depth ? null : 'none'; })
-        .attr('d', varArc)
-        .style('stroke', '#fff')
-        .style('fill', function (d) {
-          return color(colors[d.data.name]);
-        })
-        .on('mouseover', this.mouseover);*/
-        console.log(this.mouseover)
+
+    let sequenceArray = this.state.hovered != null ? this.getAncestors(this.state.hovered) : [];
+
     return (
+
       <div className="visContainer">
         <span id="test-topic"></span>
         <span id="proficiency-level"></span>
@@ -94,6 +80,7 @@ class Visualization2 extends Component {
         <span id="percentage"></span>
         <br/>
         <svg
+          className="vis2svg"
           width={ this.props.width }
           height={ this.props.height }
           viewBox={`0 0 ${this.props.width} ${this.props.height}`}
@@ -101,12 +88,16 @@ class Visualization2 extends Component {
           <g className="vis2"
             transform={"translate(" + this.props.width/2 + "," + this.props.height/2 + ")"}>
             {
-              root.descendants().map((d,i) => (
+              this.state.root.descendants().map((d,i) => (
                 <path
-                  key={ `path-${ i }` }
-                  display={function (d) { return d.depth ? null : 'none'; }}
-                  d={arcGenerator(d)}
+                  key = { `path-${ i }` }
+                  display = {function (d) { return d.depth ? null : 'none'; }}
+                  d = {arcGenerator(d)}
                   onMouseOver = {() => {this.mouseover(d)}}
+                  fill = {color(colors[d.data.name])}
+                  opacity = {
+                    sequenceArray.indexOf(d) >= 0 ? 1.0 : 0.3
+                  }
                 />
               ))
             }
@@ -170,17 +161,10 @@ class Visualization2 extends Component {
          .text(d.parent.data.name)
        select('#school-type')
      }
-
-     // Fade all the segments.
-     select('svg').selectAll('path')
-         .style('opacity', 0.3);
-
-     // Then highlight only those that are an ancestor of the current segment.
-     select('svg').select('g').selectAll('path')
-         .filter(function(node) {
-                   return (sequenceArray.indexOf(node) >= 0);
-                 })
-         .style('opacity', 1);
+     this.setState({
+       hovered: d
+     })
+     this.render()
    }
 
 }
